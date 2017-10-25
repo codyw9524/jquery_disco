0f1290bd99b9f8f5e327538107683ed4ec985328
codyw9524 <codyw9524@gmail.com>
2017-10-25 11:22:19 -0500

    updated app.js and index.html

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-$(function(){
-	console.log('jQuery is working!');
+$(function() {
 	var discoPower = null;
 	$('#disco').click(function(){
 		if(discoPower === null){
@@ -8,24 +7,24 @@ $(function(){
 					let color = getColor();
 					$(this).css('background-color', color);
 				})
-			},100)
+			}, 100);
 		} else {
 			clearInterval(discoPower);
 			discoPower = null;
 		}
-	})
+	});
 	$('.cell').hover(function(){
 		let color = getColor();
 		$(this).css('background-color', color);
-	})
+	});
 	$('#reset').click(function(){
 		$('.cell').css('background-color', '');
-	})
-})
+	});
+});
 
 function getColor(){
-	colorsArray = ["#FFFF00", "#FFFF33", "#F2EA02", "#E6FB04", "#FF0000", "#FD1C03", "#FF3300", "#FF6600", "#00FF00", "#00FF33", "#00FF66", "#33FF00", "#00FFFF", "#099FFF", "#0062FF", "#0033FF", "#FF00FF", "#FF00CC", "#FF0099", "#CC00FF", "#9D00FF", "#CC00FF", "#6E0DD0", "#9900FF"];
-	var length = colorsArray.length;
-	var randomIndex = Math.floor(Math.random() * length);
+	let colorsArray = ["#FFFF00", "#FFFF33", "#F2EA02", "#E6FB04", "#FF0000", "#FD1C03", "#FF3300", "#FF6600", "#00FF00", "#00FF33", "#00FF66", "#33FF00", "#00FFFF", "#099FFF", "#0062FF", "#0033FF", "#FF00FF", "#FF00CC", "#FF0099", "#CC00FF", "#9D00FF", "#CC00FF", "#6E0DD0", "#9900FF"];
+	let length = colorsArray.length;
+	let randomIndex = Math.floor(Math.random() * length);
 	return colorsArray[randomIndex];
 }
